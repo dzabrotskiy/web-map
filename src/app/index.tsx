4f560d9ed7b7210adceb5c191e99e7b3cf69ea30
Daniil <dzabrotskiy@yandex.ru>
2020-08-20 19:08:20 +0300

    add 2gis tile layer

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,23 +1,26 @@
 import React from 'react'
 import 'ol/ol.css'
 import Map from 'ol/Map'
-import OSM from 'ol/source/OSM'
+// import OSM from 'ol/source/OSM'
 import TileLayer from 'ol/layer/Tile'
 import View from 'ol/View'
 import css from './index.css'
 import {fromLonLat} from 'ol/proj'
 import {Zoom} from './components/zoom'
 import {SearchPanel} from './components/SearchPanel'
+import XYZSource from 'ol/source/XYZ'
 
 export const App = (): JSX.Element => {
   const [map, setMap] = React.useState<Map | null>(null)
   React.useEffect(() => {
+    const layer2GIS = new TileLayer({
+      source: new XYZSource({
+        url: 'http://tile1.maps.2gis.com/tiles?layerType=nc&x={x}&y={y}&z={z}',
+        crossOrigin: 'anonymous'
+      })
+    })
     const map = new Map({
-      layers: [
-        new TileLayer({
-          source: new OSM(),
-        }),
-      ],
+      layers: [layer2GIS],
       target: 'map',
       view: new View({
         center: fromLonLat([37.6178, 55.7517]),
