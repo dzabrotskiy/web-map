a7a0d151e09b5ff8b62146901dfc9f5d151422b2
Daniil <dzabrotskiy@yandex.ru>
2020-08-20 19:08:50 +0300

    remove unused import

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import 'ol/ol.css'
 import Map from 'ol/Map'
-// import OSM from 'ol/source/OSM'
 import TileLayer from 'ol/layer/Tile'
 import View from 'ol/View'
 import css from './index.css'
